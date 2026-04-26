5d65fefd4a244073bbb10cd8140f0608eb15120b
loki <gunlinux@ya.ru>
2026-04-26 19:13:47 +0300

    pasha is back

--- local_events/usecases/help.py
+++ local_events/usecases/help.py
@@ -8,22 +8,16 @@ logger = logger_setup(__name__)
 
 
 @dataclass
-class FlashbackUsecase(BaseUseCase):
-    duration: int = 30
+class HelpUsecase(BaseUseCase):
+    duration: int = 8
     obs_client: ObsClient = field(default_factory=ObsClient)
 
     async def start(self) -> None:
         self.obs_client.connect()
-        self.obs_client.set_source_filter_enabled(
-            source_name='webcam_full', filter_name='gray'
-        )
-        self.obs_client.scene_item_enabled('desktop', 'flashback')
+        self.obs_client.scene_item_enabled('desktop', 'help')
         self.obs_client.disconnect()
 
     async def finish(self) -> None:
         self.obs_client.connect()
-        self.obs_client.set_source_filter_disabled(
-            source_name='webcam_full', filter_name='gray'
-        )
-        self.obs_client.scene_item_disabled('desktop', 'flashback')
+        self.obs_client.scene_item_disabled('desktop', 'help')
         self.obs_client.disconnect()
